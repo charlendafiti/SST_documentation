5e011341579c235fb5b1bd7ad2dd1979b6b70452
Rafael Barreto <rafael.barreto@dafiti.com.br>
2022-06-15 14:32:29 -0300

    Initial styles

--- static/js/index.js
+++ static/js/index.js
@@ -5,24 +5,23 @@ fetch('./getTasks/').
                 let currentTask = document.createElement('div');
                 let {id, title, description, dev_journey, SEO_principles, best_practices} = task;
 
-            currentTask.className = "task";
+                currentTask.className = "task";                
+
+                let taskHtml = `
+                    <div class="task-header">
+                        <h2 class="task-title">${id} - ${title}</h2>
+                    </div>
+                    <div class="task-body">
+                        <h3>Description</h3><div class="task-description">${description||""}</div>
+                        <h3>Dev Journey</h3><div class="task-description">${dev_journey||""}</div>
+                        <h3>SEO Principles discovered</h3><div class="task-description">${SEO_principles||""}</div>
+                        <h3>Best Practices</h3><div class="task-description">${best_practices||""}</div>
+                    </div>
+                `; 
                 
-            // # id - title
-            currentTask.innerHTML = `<h2 class="task-title">${id} - ${title}</h2>`;
-
-            // # description
-            currentTask.innerHTML += `<h3>Description</h3><div class="task-description">${description||""}</div>`;
-
-            // # dev journey
-            currentTask.innerHTML += `<h3>Dev Journey</h3><div class="task-description">${dev_journey||""}</div>`;
-
-            // # SEO Principles discovered
-            currentTask.innerHTML += `<h3>SEO Principles discovered</h3><div class="task-description">${SEO_principles||""}</div>`;
-
-            // # Best Practices
-            currentTask.innerHTML += `<h3>Best Practices</h3><div class="task-description">${best_practices||""}</div>`;
-            
-            document.body.append( currentTask );
+                currentTask.innerHTML = taskHtml; 
+                
+                document.body.append( currentTask );
 
             });
         })
