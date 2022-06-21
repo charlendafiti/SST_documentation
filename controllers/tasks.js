b4c1a0f7f60d29e6b440b6e4bf6a602a52b1ac19
Rafael Barreto <rafael.barreto@dafiti.com.br>
2022-06-21 16:05:41 -0300

    Merge with master

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -2,21 +2,11 @@ const fs = require('fs');
 const database = require('../services/database');
 function TaskController() {
 
-<<<<<<< HEAD
-    this.getTasks = function(req, res){
-        res.send(this.tasks);
-    }
-    
-    this.getTask = function(req, res) {
-        res.send(this.tasks.filter(task => task.id == req.params.id));
-    }
-=======
     this.getTasks = async function(req, res){
         
         let tasks = await database.getAllTasks();
         res.send(tasks);
     }    
->>>>>>> master
 
     this.updateTask = async function(req, res){
         let currentTaskIdx;
