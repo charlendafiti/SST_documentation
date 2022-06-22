6e153a0e4d1c29b0bda84b28eed5a0bba5e8636b
Charlen Silva <charlen.silva@dafiti.com.br>
2022-06-22 18:00:43 -0300

    add checkToken middleware

---
+++ middleware/checkToken.js
@@ -0,0 +1,9 @@
+function checkToken(req, res, next){
+    if(req.headers.token == 'c186c30450a61d9b3ac71748ac9b866bb477e945'){
+        next();
+    } else {
+        res.status(401).send({error: true, message: 'Unauthorized'});
+    }
+}
+
+module.exports = checkToken;
--- routes/tasks.js
+++ routes/tasks.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const fs = require('fs');
 const TaskController = require('../controllers/tasks')
+const checkToken = require('../middleware/checkToken');
 
 const router = express.Router();
+
+router.use(checkToken);
 
 router.get('/', TaskController.getTasks);
 router.post('/', TaskController.updateTask);
