4e2dd0ec21f210a5782a523ce03cb01f7b42eb7d
Charlen Silva <charlen.silva@dafiti.com.br>
2022-06-22 15:10:02 -0300

    fix not found error when call /task/<id>

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const fs = require('fs');
 const taskRoutes = require('./routes/tasks');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 
@@ -21,6 +22,8 @@ const port = process.argv[2] || 3021;
 app.use(express.static('./front/dist/'));
 
 app.use('/tasks', taskRoutes);
+
+app.use(/\/task\/.*/, express.static('./front/dist/'));
 
 app.get('/getTasks1/', (req, res) => {
     let mockTask = fs.readFileSync('./tasks.json', {encoding: 'utf-8'});
