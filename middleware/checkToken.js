4c054408ae2cae52d99cd1ef9a66e02cc347b6ba
Charlen Silva <charlen.silva@dafiti.com.br>
2022-06-23 10:32:14 -0300

    disable token temporaly

--- middleware/checkToken.js
+++ middleware/checkToken.js
@@ -1,9 +1,10 @@
 function checkToken(req, res, next){
-    if(req.headers.token == 'c186c30450a61d9b3ac71748ac9b866bb477e945'){
-        next();
-    } else {
-        res.status(401).send({error: true, message: 'Unauthorized'});
-    }
+    // if(req.headers.token == 'c186c30450a61d9b3ac71748ac9b866bb477e945'){
+    //     next();
+    // } else {
+    //     res.status(401).send({error: true, message: 'Unauthorized'});
+    // }
+    next();
 }
 
 module.exports = checkToken;
